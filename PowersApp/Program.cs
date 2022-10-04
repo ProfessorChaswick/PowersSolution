92f95eb7b23708b7f3116b845cf335e7f9119cc4
Charles Hildebrant <ceh2624@gmail.com>
2022-10-04 11:15:34 -0400

    Add project files.

---
+++ PowersApp/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    public static int PowerRanger(int power, int min, int max)
+    {
+        int numOfPows = 0;
+        double Pows = (double)power;
+        Pows = 1d / Pows;
+        for(int x = min; x <= max; x++)
+        {
+            if(Math.Ceiling(Math.Pow(x, Pows)) ==
+                Math.Floor(Math.Pow(x, Pows)))
+                numOfPows++;
+        }
+        return numOfPows;
+    }
+    public static void Main()
+    {
+        Console.WriteLine("The answer is {0}", PowerRanger(2,1,100));
+    }
+}
